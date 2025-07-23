26c3e3ef258141a461aa57378573fc89f2d277d3
Dharneesh-009 <dharaneeshprabhakaran@gmail.com>
2025-07-23 15:16:04 +0530

    adding docker-file

--- kanban-backend/src/main/java/com/virutualtask/kanban_backend/KanbanBackendApplication.java
+++ kanban-backend/src/main/java/com/virutualtask/kanban_backend/KanbanBackendApplication.java
@@ -2,7 +2,7 @@ package com.virutualtask.kanban_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration; // ✅ optional if you’re customizing everything
+
 
 @SpringBootApplication
 public class KanbanBackendApplication {
